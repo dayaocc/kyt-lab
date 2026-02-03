ab7d523b6d3df432cd27e30f80729b5f09ac3290
dayaocc <ccdaruiyao@gmail.com>
2026-02-03 18:27:24 +0800

    update

--- src/scripts/notifier.py
+++ src/scripts/notifier.py
@@ -1,4 +1,7 @@
 import requests
+import smtplib  #发送邮件的标准库模块
+from email.mime.text import MIMIText    # 用于构造邮件正文
+from email.mime.multipart import MIMEMultipart  # 用于构造带附件的邮件
 
 class TelegramBot:
     def __init__(self, token, chat_id): 
@@ -20,3 +23,27 @@ class TelegramBot:
             requests.post(url, data=data, files=files)
             print("图片汇总已推送到 Telegram")
 
+class EmailNotifier:
+    def __init__(self, smtp_config):
+        # 从配置中读取服务器地址、端口、发件人账号和授权码
+        self.server = smtp_config['server']
+        self.port = smtp_config['port']
+        self.user = smtp_config['user']
+        self.password = smtp_config['password']
+    def send_html_report(self, receiver, subject, html_content, attachment_path=None):    # receiver 收件人地址, subject 邮件主题, content 邮件正文, attachment_path 附件文件路径
+        # 1.准备邮件容器
+        msg = MIMEMultipart()
+        msg['From'] = self.user     # 设置邮件头-发件人
+        msg['To'] = receiver    # 设置邮件头-收件人
+        msg['Subject'] = subject    # 设置邮件头-主题
+        # 2.添加邮件正文
+        msg.attach(MIMIText(html_content, 'html'))  # 邮件协议要求每一块内容必须声明类型，plain表示纯文本。一般转换为html更好看
+        # 3.添加附件（如果有）
+        # if attachment_path:
+        #     with open(attachment_path, 'rb') as f:
+        #         part = MIMEMultipart(f.read())
+        #         part.add_header()
+        # pass # 占位符，代码逻辑未完成。保证程序能跑通
+        with smtplib.SMTP_SSL(self.server, self.port) as smtp:
+            smtp.login(self.user, self.password)
+            smtp.send_message(msg)
--- src/scripts/reporter.py
+++ src/scripts/reporter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 import datetime
 
+
 # 为了不让程序崩溃，我们需要给图表设置一个固定的保存路径。
 class RiskReporter:
     def __init__(self, output_dir="outputs"):
@@ -46,3 +47,33 @@ class RiskReporter:
         path = os.path.join(self.output_dir, filename)
         df.to_excel(path, index=False)
         return path
+    
+    def get_simple_html_report(self, df):
+        # justify='center' 让内容居中
+        # classes 允许我们后续通过 CSS 给名称为为risk_table的表格加样式,让表格更好看
+        return df.to_html(index=False, justify='center', classes='risk_table')
+
+    # 新增方法：生成(内嵌CSS）HTML 格式的报告
+    def get_styled_html_report(self, df): 
+
+        # 1.定义高亮函数，如果风险等级是 Critical，整行变红
+        # axis=1：按行处理。故row代表DataFrame中的每一行数据，pandas会自动传入
+        def highlight_critical(row):   
+            # 这里的样式：浅红色背景 + 深红色加粗文字
+            critical_style = 'background-color: #ffe6e6; color: #b30000; font-weight: bold;'
+            if row['risk_level'] == 'Critical':
+                return [critical_style] * len(row)  # 整行的每一列都应用这个样式
+            return [''] * len(row)  # 如果不是 'Critical'，就返回空字符串列表
+
+        # 2.应用样式并格式化
+        # df.style ，添加DataFrame的样式。.apply()应用到某个函数.format()控制数字的样式
+        style_df = df.style.apply(highlight_critical, axis=1).format({
+            'eth_value': 'U{:.2f}',     # {}为数据占位符。金额前加U，保留2位小数
+            'total_risk_score': '{:.0f} pts'    # .0f 保留0位小数（即取整）。pts为单位
+        })
+
+        # 3.转化为带样式（内嵌CSS）的 HTML
+        #注意：Styler 的 to_html 会包含 CSS 代码，确保邮件客户端能识别
+        return style_df.to_html()
+        
+        
